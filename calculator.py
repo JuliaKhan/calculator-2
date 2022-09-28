17f7058e2f09f3d6e1c31a3e17ca6203e393acee
“Julia <juliaabkhan@gmail.com>
2022-09-28 16:59:04 -0500

    Expanded docstring and added comments.

--- calculator.py
+++ calculator.py
@@ -1,20 +1,30 @@
-"""CLI application for a prefix-notation calculator."""
+"""CLI application for a prefix-notation calculator.
+
+Quit:       'q' or 'quit' exits the application
+Add:        '+ num1 num2' prints num1 + num2
+Subtract:   '- num1 num2' prints num1 - num2
+Multiply:   '* num1 num2' prints num1 * num2
+Divide:     '/ num1 num2' prints num1 / num2
+Square:     'square num1' prints num1 ** 2
+Cube:       'cube num1' prints num1 ** 3
+Power:      'power num1 num2' prints num1 ** num2
+Modulus:    'mod num1 num2' prints num1 % num2
+"""
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-
-while True:
-    user_input = input("Enter your equation. ") # + 1 2
-    tokens = user_input.split(' ')              #['+', '1', '2']
+while True:                                     #begins REPL
+    user_input = input("Enter your equation. ") #eg input:  + 1 2
+    tokens = user_input.split(' ')              #tokenized: ['+', '1', '2']
 
     if tokens[0] == 'q' or tokens[0] == 'quit': #exit conditions
         print("Exiting calculator.")
         break
 
-    elif tokens[0] == '+':
-        try:
-            print(add(float(tokens[1]), float(tokens[2])))
+    elif tokens[0] == '+':  #checks first item of tokens for operation
+        try:                #exception handling for non-float inputs
+            print(add(float(tokens[1]), float(tokens[2]))) #print answer
         except:
             print("invalid operation")
 
@@ -59,5 +69,5 @@ while True:
             print(mod(float(tokens[1]),float (tokens[2])))
         except:
             print("invalid operation")
-            
-    else: print("invalid operation")
+
+    else: print("invalid operation")  #exception handling for invalid operation
